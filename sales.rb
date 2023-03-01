fe920763985d8551dbd163ff897ed40889d07ed0
athira <athira.ks@chordify.com>
2023-03-01 13:01:10 +0530

    sales

--- sales.rb
+++ sales.rb
@@ -15,49 +15,37 @@ s = [
 
   { "n"=> "Bob", "s1"=> 400,"s2"=>400,"c"=>  30},
 
-  { "n"=> "Charlie", "s1"=> 500,"s2"=>300,"c"=> 10},
+  { "n"=> "Charlie", "s1"=> 500,"s2"=>500,"c"=> 10},
 
   { "n"=> "Dave", "s1"=> 800,"s2"=>500,"c"=> 30 },
 
 ]
-puts "1....Total sales"
+result={}
+
+
 
 s.each do |i|
-    #puts "#{i['s2']}   #{i['c']}"
+    result[:name] = i['n']
     name=i['n']
     actual=i['s2']
     comm=i['c']
     target=i['s1']
+    
     t=actual+comm
-    #per=(actual/target*100)
-    
-    puts  "#{name} = #{t}"
-   # puts  "percentage of sales #{name} = #{per}"
-end   
-puts  "2..percentage of sales "
-
-s.each do |i|
-    name=i['n']
-    actual=i['s2']
-    target=i['s1']
-   
+    result[:total_sales] = t
     per=(actual/target*100)
-    puts  "percentage of sales #{name} = #{per}"
+    result[:sales_percentage] = per 
+    if actual==target
+        result[:target_met_key] = "True"
+    else
+      result[:target_met_key] = "False"
+    end   
 
+
+    puts result
     
-end   
+end  
 
 
-
-puts "3..names of the employees who did not meet their sales target are "
-
-s.each do |i|
-  t=i['s1']
-  a=i['s2']
-  if t!=a
-    name= i['n']
-    puts name
-end
-end
 
 
