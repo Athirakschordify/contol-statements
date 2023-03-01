a08f374b87871ec647d692c75e014b2f72d16339
athira <athira.ks@chordify.com>
2023-03-01 12:15:47 +0530

    book

--- book_hash.rb
+++ book_hash.rb
@@ -1,16 +1,8 @@
-# Mike has a list of books that he wants to keep track of. He wants to store the name, author, and year of publication for each book in a hash. He also wants to be able to search for a book by its name and update its year of publication. 
-
-# Help Mike write a Ruby program to solve this problem using hash operations. 
-
-
-
 books = {"To Kill a Mockingbird" => { author: "Harper Lee", year: 1960 },
          "1984" => { author: "George Orwell", year: 1949 },
          "The Catcher in the Rye" => { author: "J.D. Salinger", year: 1951 },
          "Pride and Prejudice" => { author: "Jane Austen", year: 1813 },
          "The Great Gatsby" => { author: "F. Scott Fitzgerald", year: 1925 }}
-         
-           
 
 
          puts "enter book name"
@@ -20,18 +12,11 @@ books = {"To Kill a Mockingbird" => { author: "Harper Lee", year: 1960 },
            puts " found successfully"
            puts "enter year"
            year1=gets.chomp.to_i
-           #books['year']=year
-           #for i in books[name]
-             #puts i
-           
-           #end
+           books[name][:year]=year1
+           puts "updated successfully"
+  
+            puts books
 
-           puts "updated"
         else
           puts " not found!"
-          end
-        for i in books[name]
-            y= i[1]
-            books[y]=year1
-        end  
-        puts books
+        end
